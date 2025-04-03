cc082226e6a8b36159934fe81c58c8fa728d5b29
KaitoMikazuki <175144170+KaitoMikazuki@users.noreply.github.com>
2025-04-03 17:55:55 +0800

    Add functions: One to get the clicked number or action, and others to perform basic arithmetic operations

--- calculator.js
+++ calculator.js
@@ -8,9 +8,48 @@ let x;
 let operation;
 let y;
 
+// Used to validate user input 
+let numbers = new Set([0,1,2,3,4,5,6,7,8,9]);
+let operations = new Set(["+","-","x","÷"]);
+let actions = new Set (["=","AC","+/-","%","."]);
+
+
+document.querySelector(".calc-keyboard").addEventListener("click", getUserInput)
+
+function getUserInput(event){
+    userInput = event.target.textContent;
+    console.log(userInput)
+    console.log(numbers.has(Number(userInput)));
+
+    if (operations.has(userInput)){
+        // be careful when they press the operator again
+    }
+    else if(actions.has(userInput)){
+
+    }
+    else if(numbers.has(Number(userInput))){
+        firstArgument = true ? x = userInput: y = userInput;
+    }
+
+
+}
+
 function operate(x,operation,y){
 
 }
 
+function add(x,y){
+    return x + y;
+}
 
+function subtract(x,y){
+    return x - y
+}
 
+function multiplication(x,y){
+    return x*y
+}
+
+function divide(x,y){
+    return x/y
+}
