fc5df9c5765614ef1c8627e696ed50f4c19f09b6
KaitoMikazuki <175144170+KaitoMikazuki@users.noreply.github.com>
2025-04-09 19:07:24 +0800

    Add display and fixed readability by abstracting from getUserInput

--- calculator.js
+++ calculator.js
@@ -1,25 +1,43 @@
-// OPERATIONS +-
-// three variables for each operand
-// operate function
-// html (buttons including clear)
-// MDAS 
+// state of clearness
+// inputvariable y indicates that both x and y are ready, all other numbers inputted will be for y
 
-let x;
-let y;
-let operation;
+
+let x = 0;
+let y = 0;
+let operation = "";
 let inputVariableY = false;
+let result = null;
 
-function main (){
-    // sets are used to validate user input 
-    let numbers = new Set([0,1,2,3,4,5,6,7,8,9]);
-    let operations = new Set(["+","-","x","÷"]);
-    let actions = new Set (["=","AC","+/-","%","."]);
+// sets are used to validate user input 
+let numbers = new Set([0,1,2,3,4,5,6,7,8,9]);
+let operations = new Set(["+","-","x","÷"]);
+let actions = new Set (["=","AC","+/-","%","."]);
 
-    document.querySelector(".calc-keyboard").addEventListener("click", getUserInput)
+document.querySelector(".calc-keyboard").addEventListener("click", getUserInput)
 
     
+function clearAll(){
+    // Resets all variables
+    [x,y,operation,inputVariableY,result] = [0,0,"",false,null]
+    displayNumber(0);
 }
 
+function highlightSelectedOperation(operation){
+    curr = document.querySelectorAll(".highlighted-operation-button")
+    console.log(curr)
+    if (curr.length > 1 ){
+        curr.classList.remove(".highlighted-operation-button")
+    }
+    ya = document.querySelector(`#${operation}`)
+    console.log(ya)
+    console.log(operation)
+    .classList.add(".highlighted-operation-button")
+}
+
+
+function displayNumber(num){
+    document.querySelector(".display-text").textContent = num;
+}
 
 
 function getUserInput(event){
@@ -30,63 +48,104 @@ function getUserInput(event){
     userInput = event.target.textContent;
     console.log(`${userInput} pressed`)
 
-
     // This series of ifs are used to validate userInput first before proceeding
     if (operations.has(userInput)){
-        if (inputVariableY == true){
-            operate()
-        }
-        operation = userInput;
+        processOperationEntered(userInput)
     }
 
     else if(actions.has(userInput)){
-        processUserAction(userInput);
+        processActionEntered(userInput);
     }
 
     else if(numbers.has(Number(userInput))){
-        processNumberEntered(userInput)
+        processNumberEntered(Number(userInput))
+        console.log(`x: ${x} y:${y}`)
     }
 }
 
 
 function operate(x,operation,y){
+
     switch(operation){
         case "+":
-            add(x,y);
+            result = add(x,y);
             break;
         case "-":
-            subtract(x,y);
+            result = subtract(x,y);
             break;
         case "x":
-            multiplication(x,y);
+            result = multiplication(x,y);
             break;
         case "÷":
-            divide(x,y);
+            result = divide(x,y);
             break;
     }
+    return
+    
+    operation = "";
+    y = 0;
+    console.log(result)
+
+    // If an operation was pressed instead of equal, proceed to the y variable 
+    if (nextOperation = ""){
+        inputVariableY = false;
+    }
+    else{
+        x = result;
+        operation = nextOperation;
+    }
+
+}
+
+function prepareForNextOperation(nextOperation){
+
+}
+
+function processOperationEntered(userInput){
+    if (inputVariableY == true && y){
+        operate(x, operation, y)
+        x = result 
+        y = 0
+        displayNumber(result)
+    }
+    else {
+        operation = userInput;
+        inputVariableY = true;
+    }
+    highlightSelectedOperation(operation);
 }
 
 
 function processNumberEntered (number){
+    if (result != null && operation == ""){
+        clearAll();
+    }
+
     // Times 10 shifts the digit to the left place value
-    if (operation != ""){
-        y += (x * 10) + number;
+    if (inputVariableY == true){
+        y = (y * 10) + number;
+        displayNumber(y);
     }
     else{
-        x += (x * 10) + number;
+        x = (x * 10) + number;
+        displayNumber(x);
     }
 }
 
-function processUserAction(action){
+function processActionEntered(action){
     switch (action){
         case "=":
-            processEquation(action);
+            if (inputVariableY == true && operation){
+                operate(x,operation,y)
+                displayNumber(result)
+            }   
+            operation = ""
             break;
         case "AC":
-            
+            clearAll();
             break;
         case "+/-":
-                    
+              
             break;
         case "%":
                         
@@ -97,8 +156,6 @@ function processUserAction(action){
     }
 }
 
-
-function updateDisplay()
 
 // Arithmethic operations from this point on
 function add(x,y){
